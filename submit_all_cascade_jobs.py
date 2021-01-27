b8290b6ba7e56ced1aa7ed82a7bd8300e30c4038
Alex Pizzuto <pizzuto@wisc.edu>
2021-01-27 15:34:52 -0600

    script for submitting jobs to the cluster

---
+++ submit_all_cascade_jobs.py
@@ -0,0 +1,96 @@
+import pycondor, argparse, sys, os.path
+from glob import glob
+import numpy as np
+import pandas as pd
+
+error = '/scratch/apizzuto/novae/condor/error'
+output = '/scratch/apizzuto/novae/condor/output'
+log = '/scratch/apizzuto/novae/condor/log'
+submit = '/scratch/apizzuto/novae/condor/submit'
+
+script_base = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/cascade_scripts'
+
+dagman_short_timescale = pycondor.Dagman(
+    'FRA_cascade_short_timescale_sens', submit=submit, verbose=2
+    )
+dagman_long_timescale = pycondor.Dagman(
+    'FRA_cascade_long_timescale_sens', submit=submit, verbose=2
+    )
+dagman_fits = pycondor.Dagman(
+    'FRA_cascade_fitting_bias', submit=submit, verbose=2
+    )
+
+background_short_time_jobs = pycondor.Job(
+    'fra_background_short_time',
+    script_base + 'cascade_alert_background.py',
+    error=error, output=output, log=log, submit=submit,
+    getenv=True, universe='vanilla',
+    verbose=2, request_memory=6000,
+    request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT'],
+    dag=dagman_short_timescale
+	)
+
+background_long_time_jobs = pycondor.Job(
+    'fra_long_short_time',
+    script_base + 'cascade_alert_background.py',
+    error=error, output=output, log=log, submit=submit,
+    getenv=True, universe='vanilla',
+    verbose=2, request_memory=8000,
+    request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT'],
+    dag=dagman_long_timescale
+	)
+
+sens_short_time_jobs = pycondor.Job(
+    'fra_sens_short_time',
+    script_base + 'cascade_alert_sensitivity.py',
+    error=error, output=output, log=log, submit=submit,
+    getenv=True, universe='vanilla',
+    verbose=2, request_memory=6000,
+    request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT'],
+    dag=dagman_short_timescale
+	)
+
+sens_long_time_jobs = pycondor.Job(
+    'fra_sens_long_time',
+    script_base + 'cascade_alert_sensitivity.py',
+    error=error, output=output, log=log, submit=submit,
+    getenv=True, universe='vanilla',
+    verbose=2, request_memory=8000,
+    request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT'],
+    dag=dagman_long_timescale
+	)
+
+fits_jobs = pycondor.Job(
+    'fra_fits_jobs',
+    script_base + 'cascade_alert_fits.py',
+    error=error, output=output, log=log, submit=submit,
+    getenv=True, universe='vanilla',
+    verbose=2, request_memory=8000,
+    request_cpus=5,
+    extra_lines= ['should_transfer_files = YES', 
+        'when_to_transfer_output = ON_EXIT'],
+    dag=dagman_fits
+	)
+
+for deltaT, bg_job, sens_job in [(1000., background_short_time_jobs, sens_short_time_jobs),
+    (2.*86400., background_long_time_jobs, sens_long_time_jobs)]:
+    for index in range(0, 62, 4):
+        bg_job.add_arg(f'--index={index} --deltaT={deltaT} --ntrails=5000')
+        sens_job.add_arg(f'--index={index} --deltaT={deltaT} --ntrials=100')
+        fits_jobs.add_arg(f'--index={index} --deltaT={deltaT} --ntrials=100')
+  
+
+background_short_time_jobs.add_child(sens_short_time_jobs)
+background_long_time_jobs.add_child(sens_long_time_jobs)
+
+dagman_short_timescale.build_submit()
+dagman_long_timescale.build_submit()
+dagman_fits.build_submit()
