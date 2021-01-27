fa5d4ad643e22dd19256c35dfc7ed345a5e5108b
Alex Pizzuto <pizzuto@wisc.edu>
2021-01-27 14:46:55 -0600

    signal trials for cascades instead of tracks

--- cascade_alert_fits.py
+++ cascade_alert_fits.py
@@ -2,57 +2,47 @@
 
 import numpy as np
 import healpy as hp
-import os, sys, argparse
-from astropy.time import Time
-from astropy.time import TimeDelta
-from numpy.lib.recfunctions import append_fields
-from astropy.io import fits
+import argparse
 from glob import glob
+from astropy.time import Time
 import pickle
 
 from fast_response.FastResponseAnalysis import FastResponseAnalysis
 
-parser = argparse.ArgumentParser(description='Fast Response Analysis')
+parser = argparse.ArgumentParser(description='FRA Cascade fits')
 parser.add_argument('--index', type=int,default=None,
                     help='skymap index')
 parser.add_argument('--deltaT', type=float, default=None,
                     help='Time Window in seconds')
 parser.add_argument('--ntrials', type=int, default = 100,
-                        help='Trials per injection strength')
-parser.add_argument('--smear', default=False, action='store_true',
-                    help='Include systematics by smearing norm. prob.')
+                        help='Trials')
 args = parser.parse_args()
 
-#skymaps_path = '/data/user/steinrob/millipede_scan_archive/fits_v3_prob_map/'
-#files = glob(skymaps_path + '*.fits')
-output_paths = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/analysis_trials/fits/'
+output_paths = '/data/user/apizzuto/fast_response_skylab/fast-response/fast_response/cascades_results/fits/'
+with open('/home/tgregoire/hese_cascades/archival_data.pkl', 'rb') as f: 
+        cascade_info = pickle.load(f, encoding='latin1') 
 
-skymap_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run1*.fits.gz')
+event_mjd = cascade_info['mjd'][args.index]
+run_id = cascade_info['run'][args.index]
+event_id = cascade_info['event'][args.index]
 
-skymap_fits, skymap_header = hp.read_map(skymap_files[args.index], h=True, verbose=False)
-skymap_header = {name: val for name, val in skymap_header}
-ev_mjd = skymap_header['EVENTMJD']
-run_id = skymap_header['RUNID']
-event_id = skymap_header['EVENTID']
+deltaT = args.deltaT / 86400.
 
-gammas = [2.5] #np.linspace(2., 3., 3)
-nsigs = [1., 2., 3., 4., 5, 6., 7., 8., 9., 10., 15., 20., 25., 30., 35., 40.]
-deltaT = args.deltaT / 86400. 
-
-#skymap_fits = fits.open(files[args.index])[0]
-#event_mjd = skymap_fits.header['TIME_MJD']
-event_mjd = ev_mjd    #58000.000 #HARDCODE SO THAT THERE IS REAL DATA
 start_mjd = event_mjd - (deltaT / 2.)
 stop_mjd = event_mjd + (deltaT / 2.)
-
 start = Time(start_mjd, format='mjd').iso
 stop = Time(stop_mjd, format='mjd').iso
+
+cascade_healpy_file = '/data/user/apizzuto/fast_response_skylab/' \
+    + 'fast-response/fast_response/cascades_results/skymaps/IceCube-Cascade' \
+    + '_{}_{}.hp'.format(int(run_id), int(event_id))
+
+gammas = [2.5] #np.linspace(2., 3., 3)
+nsigs = [1., 2., 3., 4., 5, 6., 7., 8., 9., 10., 15., 20.]
 
 trials_per_sig = args.ntrials
 
 tsList_prior  = []
-tsList        = []
-nsList        = []
 nsList_prior  = []
 true_ns       = []
 ra            = []
@@ -66,8 +56,8 @@ true_decs     = []
 seed_counter = 0
 
 for gamma in gammas:
-    f = FastResponseAnalysis(skymap_files[args.index], start, stop, save=False, 
-                alert_event=True, smear=args.smear, alert_type='track')
+    f = FastResponseAnalysis(cascade_healpy_file, start, stop, save=False, 
+                alert_event=True, smear=False, alert_type='cascade')
     inj = f.initialize_injector(gamma=gamma)
     scale_arr = []
     step_size = 10
@@ -92,11 +82,8 @@ for gamma in gammas:
                         spatial_prior = f.spatial_prior, time_mask = [deltaT / 2., event_mjd],
                         pixel_scan = [f.nside, 4.0], inject = sample)
                 tsList_prior.append(val['TS_spatial_prior_0'].max())
-                tsList.append(val['TS'].max())
                 max_prior   = np.argmax(val['TS_spatial_prior_0'])
-                max_noPrior = np.argmax(val['TS'])
                 nsList_prior.append(val['nsignal'][max_prior])
-                nsList.append(val['nsignal'][max_noPrior])
                 true_ns.append(val['n_inj'][max_prior])
                 ra.append(val['ra'][max_prior])
                 dec.append(val['dec'][max_prior])
@@ -107,9 +94,7 @@ for gamma in gammas:
                 true_decs.append(true_dec[0])
             except ValueError:
                 tsList_prior.append(0.0)
-                tsList.append(0.0)
                 nsList_prior.append(0.0)
-                nsList.append(0.0)
                 true_ns.append(0.0)
                 ra.append(0.0)
                 dec.append(0.0)
@@ -119,11 +104,10 @@ for gamma in gammas:
                 true_ras.append(true_ra[0])
                 true_decs.append(true_dec[0])
 
-results = {'ts_prior': tsList_prior, 'ts': tsList, 'ns_prior': nsList_prior,
-            'ns': nsList, 'true_ns': true_ns, 'ra': ra, 'dec': dec,
+results = {'ts_prior': tsList_prior, 'ns_prior': nsList_prior,
+            'true_ns': true_ns, 'ra': ra, 'dec': dec,
             'gamma': gammaList, 'mean_ninj': mean_ninj, 'flux': flux_list,
             'true_ra': true_ras, 'true_dec': true_decs}
 
-smear_str = 'smeared/' if args.smear else 'norm_prob/'
-with open(output_paths + smear_str + 'index_{}_run_{}_event_{}_time_{}.pkl'.format(args.index, run_id, event_id, args.deltaT), 'w') as fi:
+with open(output_paths + 'index_{}_run_{}_event_{}_time_{}.pkl'.format(args.index, run_id, event_id, args.deltaT), 'wb') as fi:
     pickle.dump(results, fi, protocol=pickle.HIGHEST_PROTOCOL)
--- cascade_alert_sensitivity.py
+++ cascade_alert_sensitivity.py
@@ -2,52 +2,49 @@
 
 import numpy as np
 import healpy as hp
-import os, sys, argparse
-from astropy.time import Time
-from astropy.time import TimeDelta
-from numpy.lib.recfunctions import append_fields
-from astropy.io import fits
+import argparse
 from glob import glob
+from astropy.time import Time
 import pickle
+
 
 from fast_response.FastResponseAnalysis import FastResponseAnalysis
 
-parser = argparse.ArgumentParser(description='Fast Response Analysis')
+parser = argparse.ArgumentParser(description='FRA Cascade sensitivity')
 parser.add_argument('--index', type=int,default=None,
                     help='skymap index')
 parser.add_argument('--deltaT', type=float, default=None,
                     help='Time Window in seconds')
 parser.add_argument('--ntrials', type=int, default = 100,
-                        help='Trials per injection strength')
-parser.add_argument('--smear', default=False, action='store_true',
-                    help='Include systematics by smearing norm. prob.')
+                        help='Trials')
 args = parser.parse_args()
 
-output_paths = '/data/user/apizzuto/fast_response_skylab/alert_event_followup/analysis_trials/sensitivity/'
+output_paths = '/data/user/apizzuto/fast_response_skylab/fast-response/fast_response/cascades_results/sensitivity/'
+with open('/home/tgregoire/hese_cascades/archival_data.pkl', 'rb') as f: 
+        cascade_info = pickle.load(f, encoding='latin1') 
 
-skymap_files = glob('/data/ana/realtime/alert_catalog_v2/fits_files/Run1*.fits.gz')
-skymap_fits, skymap_header = hp.read_map(skymap_files[args.index], h=True, verbose=False)
-skymap_header = {name: val for name, val in skymap_header}
-ev_mjd = skymap_header['EVENTMJD']
-run_id = skymap_header['RUNID']
-event_id = skymap_header['EVENTID']
+event_mjd = cascade_info['mjd'][args.index]
+run_id = cascade_info['run'][args.index]
+event_id = cascade_info['event'][args.index]
+
+deltaT = args.deltaT / 86400.
+
+start_mjd = event_mjd - (deltaT / 2.)
+stop_mjd = event_mjd + (deltaT / 2.)
+start = Time(start_mjd, format='mjd').iso
+stop = Time(stop_mjd, format='mjd').iso
+
+cascade_healpy_file = '/data/user/apizzuto/fast_response_skylab/' \
+    + 'fast-response/fast_response/cascades_results/skymaps/IceCube-Cascade' \
+    + '_{}_{}.hp'.format(int(run_id), int(event_id))
 
 gammas = [2.5] #np.linspace(2., 3., 3)
 nsigs = [1., 2., 3., 4., 6., 8., 10., 15., 20., 25., 30., 50.]
 deltaT = args.deltaT / 86400.
 
-event_mjd = ev_mjd  #58000.000 #HARDCODE SO THAT THERE IS REAL DATA
-start_mjd = event_mjd - (deltaT / 2.)
-stop_mjd = event_mjd + (deltaT / 2.)
-
-start = Time(start_mjd, format='mjd').iso
-stop = Time(stop_mjd, format='mjd').iso
-
 trials_per_sig = args.ntrials
 
 tsList_prior  = []
-tsList        = []
-nsList        = []
 nsList_prior  = []
 true_ns       = []
 ra            = []
@@ -61,8 +58,8 @@ true_decs     = []
 seed_counter = 0
 
 for gamma in gammas:
-    f = FastResponseAnalysis(skymap_files[args.index], start, stop, save=False, 
-                        alert_event=True, smear=args.smear, alert_type='track')
+    f = FastResponseAnalysis(cascade_healpy_file, start, stop, save=False, 
+                        alert_event=True, smear=False, alert_type='cascade')
     inj = f.initialize_injector(gamma=gamma)
     scale_arr = []
     step_size = 10
@@ -87,11 +84,8 @@ for gamma in gammas:
                         spatial_prior = f.spatial_prior, time_mask = [deltaT / 2., event_mjd],
                         pixel_scan = [f.nside, 4.0], inject = sample)
                 tsList_prior.append(val['TS_spatial_prior_0'].max())
-                tsList.append(val['TS'].max())
                 max_prior   = np.argmax(val['TS_spatial_prior_0'])
-                max_noPrior = np.argmax(val['TS'])
                 nsList_prior.append(val['nsignal'][max_prior])
-                nsList.append(val['nsignal'][max_noPrior])
                 true_ns.append(val['n_inj'][max_prior])
                 ra.append(val['ra'][max_prior])
                 dec.append(val['dec'][max_prior])
@@ -102,9 +96,7 @@ for gamma in gammas:
                 true_decs.append(true_dec[0])
             except ValueError:
                 tsList_prior.append(0.0)
-                tsList.append(0.0)
                 nsList_prior.append(0.0)
-                nsList.append(0.0)
                 true_ns.append(0.0)
                 ra.append(0.0)
                 dec.append(0.0)
@@ -114,11 +106,10 @@ for gamma in gammas:
                 true_ras.append(true_ra[0])
                 true_decs.append(true_dec[0])
 
-results = {'ts_prior': tsList_prior, 'ts': tsList, 'ns_prior': nsList_prior,
-            'ns': nsList, 'true_ns': true_ns, 'ra': ra, 'dec': dec,
+results = {'ts_prior': tsList_prior, 'ns_prior': nsList_prior,
+            'true_ns': true_ns, 'ra': ra, 'dec': dec,
             'gamma': gammaList, 'mean_ninj': mean_ninj, 'flux': flux_list,
             'true_ra': true_ras, 'true_dec': true_decs}
 
-smear_str = 'smeared/' if args.smear else 'norm_prob/'
-with open(output_paths + smear_str + 'index_{}_run_{}_event_{}_time_{}.pkl'.format(args.index, run_id, event_id, args.deltaT), 'w') as fi:
+with open(output_paths + 'index_{}_run_{}_event_{}_time_{}.pkl'.format(args.index, run_id, event_id, args.deltaT), 'wb') as fi:
     pickle.dump(results, fi, protocol=pickle.HIGHEST_PROTOCOL)
